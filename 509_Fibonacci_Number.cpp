e5a6c0632e0ada942cc87479f3a25b42ec9994e3
Parth Deshpande <parth12w@gmail.com>
2024-09-26 21:38:12 +0530

    Solution for easy questions of Leet Code

---
+++ 509_Fibonacci_Number.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+
+
+    int fib(int n) 
+    {
+        for(int i=0;i<=n;i++)
+        {
+            if(n==0)
+            {
+                return 0;
+            }
+            else if(n==1)
+            {
+                return 1;
+            }
+            else
+            {
+                return fib(n-1)+fib(n-2);
+            }
+        } 
+        return {};
+    }
+};
